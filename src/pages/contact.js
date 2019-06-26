affa6426711ffff1bd2aa735f3f3d7b3ef3899bc
Florencia Lafuente <lafuente.florencia@gmail.com>
2019-06-26 17:26:38 -0300

    Add basic content to some pages

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <div>
+    Contact
+  </div>
+)
+
+export default ContactPage
